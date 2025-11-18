38dbc9f26b94f11ab1a42869d86afd35b2179aa5
Gerry <madeiramateus@gmai.com>
2025-11-18 21:44:46 +0000

    Criei a tabela 2 turmas.

---
+++ 2_grm_turmas.sql
@@ -0,0 +1,11 @@
+USE grm_system;
+
+CREATE TABLE turmas (
+    id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+    id_curso INT(11) UNSIGNED NOT NULL,
+    nome VARCHAR(100) NOT NULL,
+    ano_inicio DATE,
+    ano_fim DATE,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_curso) REFERENCES cursos(id)
+);
