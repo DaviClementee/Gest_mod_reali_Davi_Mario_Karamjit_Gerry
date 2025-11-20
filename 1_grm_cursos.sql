a3996a53120375bafade351ff627d77e827e003d
Gerry <madeiramateus@gmai.com>
2025-11-20 15:41:36 +0000

    Creiei a tabela 3 alunos.

--- 1_grm_cursos.sql
+++ 1_grm_cursos.sql
@@ -4,5 +4,6 @@ CREATE TABLE cursos (
     id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
     nome VARCHAR(100) NOT NULL,
     data_inicio DATE,
-    data_conclusao DATE
-)
+    data_conclusao DATE,
+    PRIMARY KEY(id)
+);
