1557493631cd857fffa4d4d9417adc9a42af46ad
Gerry <madeiramateus@gmai.com>
2025-11-20 15:49:07 +0000

    Implementaçao de atributos na tabala 3 alunos.

--- 3_grm_alunos.sql
+++ 3_grm_alunos.sql
@@ -1,2 +1,11 @@
 USE grm_system;
 
+CREATE TABLE alunos (
+    id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+    id_turma INT(11) UNSIGNED NOT NULL,
+    nome VARCHAR(100) NOT NULL,
+    num_processo INT NOT NULL,
+    email VARCHAR(100),
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_turma) REFERENCES turmas(id)
+);
