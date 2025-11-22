976ee4c46c03a95479723c51eb964ba26f88eed1
karam841 <gaurav2008karamjit@gmail.com>
2025-11-22 13:47:27 +0000

    Crie tabela mod_disciplina_cursos e tabela modCapatilizados.

---
+++ 6_grm_mod_disciplina_cursos.sql
@@ -0,0 +1,17 @@
+USE grm_system;
+
+DROP TABLE IF EXISTS mod_disciplina_cursos;
+
+CREATE TABLE mod_disciplina_cursos (
+    id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+    id_modulo INT(11) UNSIGNED NOT NULL,
+    id_disciplina INT(11) UNSIGNED NOT NULL,
+    id_curso INT(11) UNSIGNED NOT NULL,
+    data_inico DATE NOT NULL,
+    data_fim DATE NOT NULL,
+    hora_mod INT UNSIGNED NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_modulo) REFERENCES modulos(id),
+    FOREIGN KEY (id_disciplina) REFERENCES disciplinas(id),
+    FOREIGN KEY (id_curso) REFERENCES cursos(id)
+);
