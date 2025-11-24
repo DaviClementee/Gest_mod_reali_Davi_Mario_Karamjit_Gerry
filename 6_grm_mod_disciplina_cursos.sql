1d96466d99a665e69311b6dc205f4bc21ec568d4
daviclmm <imdavibako@gmail.com>
2025-11-24 20:15:48 +0000

    Alteraçoes na tabela mod_disciplinas_cursos, porque estava mal escrito algumas partes do codigo.

--- 6_grm_mod_disciplina_cursos.sql
+++ 6_grm_mod_disciplina_cursos.sql
@@ -7,11 +7,11 @@ CREATE TABLE mod_disciplina_cursos (
     id_modulo INT(11) UNSIGNED NOT NULL,
     id_disciplina INT(11) UNSIGNED NOT NULL,
     id_curso INT(11) UNSIGNED NOT NULL,
-    data_inico DATE NOT NULL,
+    data_inicio DATE NOT NULL,
     data_fim DATE NOT NULL,
-    hora_mod INT UNSIGNED NOT NULL,
+    horas_mod INT UNSIGNED NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (id_modulo) REFERENCES modulos(id),
     FOREIGN KEY (id_disciplina) REFERENCES disciplinas(id),
     FOREIGN KEY (id_curso) REFERENCES cursos(id)
-);  
+);
